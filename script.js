bcce78eabd9d0e863aa743bd6483649f3017565c
Kevin <Tor000020@gmail.com>
2017-06-12 22:47:29 -0400

    c
    c

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-
 const btns = document.querySelectorAll('button')
-
-
-
 for(let i = 0; i<btns.length; i++){
-  
-
     btns[i].addEventListener('click', function(){
       data = this.dataset.png;
     //         document.body.style.backgroundImage=`url("textures/${data}");`
-console.log(typeof(this.dataset.png));
 
-        
                 document.body.style.backgroundImage=`url("textures/${data}")`;
 }
     )}
+
+gist = document.querySelector('.gist');
+
+document.body.addEventListener('click', function(){
+    // console.log('working');
+    // gist.style.display='none';
+gist.classList.toggle('hide');
+
+})
